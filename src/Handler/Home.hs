3189443e6be858a632f06e5ffd744380f6e36804
Newton Nunes <newtonnunes@hotmail.com>
2018-11-26 12:27:09 +0000

    Início do front, home e navbar, modificação jquery

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -10,19 +10,28 @@ import Import
 import Text.Lucius
 import Text.Julius
 import Prelude (read)
+            
+widgetNav :: Widget
+widgetNav = $(whamletFile "templates/navbar.hamlet")
 
 widgetFooter :: Widget
 widgetFooter = $(whamletFile "templates/footer.hamlet")
 
 getHomeR :: Handler Html
 getHomeR = do 
-    msg <- getMessage
-    sess <- lookupSession "_USR"
+  --  msg <- getMessage
+  --  sess <- lookupSession "_USR"
     defaultLayout $ do 
+        setTitle "AvataR"
         toWidgetHead [hamlet|
-            <script src=@{StaticR js_jquery331_js}>
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         |]
         addStylesheet $ StaticR css_bootstrap_css
-        $(whamletFile "templates/home.hamlet")
         toWidget $(luciusFile "templates/home.lucius")
-        toWidgetHead $(juliusFile "templates/home.julius")
+        toWidgetBody $(juliusFile "templates/home.julius")
+        $(whamletFile "templates/home.hamlet")
+        toWidget [hamlet|
+            <script src=@{StaticR js_bootstrap_js}>
+            <script src=@{StaticR js_jquery_js}>
+        |]
+    
